73c50dfbe2c512ad20af949d9b0e885753c2e944
Artem Beltiukov <Artem_Beltiukov@epam.com>
2021-03-02 18:04:20 +0400

    fixed sout in main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,12 +2,12 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        Logger logger = new Logger();
+        Logger logger = LoggerFactory.getLogger();
         try {
             logger.log();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println("keks");
+        System.out.println("kekw");
     }
 }
