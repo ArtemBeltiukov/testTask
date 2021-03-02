0b3a6b5425b022de7ea3c06ec8c56e549978a818
Artem Beltiukov <Artem_Beltiukov@epam.com>
2021-03-02 18:18:13 +0400

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,6 +8,6 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println("keks");
+        System.out.println("kekw");
     }
 }
